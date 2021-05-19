0ba962acd50964981311047281e39972adcfc520
Ghariq <49440010+Ghariq@users.noreply.github.com>
2021-05-19 11:09:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     return a.exec();
-    //test
-    std::cout << "Hello World" << std::endl;
+    //test2
+    //std::cout << "Hello World" << std::endl;
 }
