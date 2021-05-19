f07e76871a7f87f3b66d17f84368f5754dd9120d
Ghariq <bapt.godard15@gmail.com>
2021-05-19 21:30:25 +0200

    Signed-off-by: Ghariq <bapt.godard15@gmail.com>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setWindowTitle("Aller let's go le WifiBot");
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,13 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::on_forward_clicked()
+{
+    QMessageBox::information(this, "title", "Hello world");
+}
+
+
+void MainWindow::on_backward_clicked()
+{
+    QMessageBox::information(this, "title", "Hello world");
+}
